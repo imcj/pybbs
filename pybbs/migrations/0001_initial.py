3f10e54c3c606d039a12dc87259bdb85e8c29dc2
cnimcj <CJ@CJ-PC>
2011-01-08 04:56:55 +0800

    调整项目。加入south应用，主要是调整了settings的配置，但是，貌似和服务器就不一样了。

---
+++ pybbs/migrations/0001_initial.py
@@ -0,0 +1,123 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'Category'
+        db.create_table('pybbs_category', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50, db_index=True)),
+            ('sort', self.gf('django.db.models.fields.IntegerField')(default=1, max_length=10)),
+        ))
+        db.send_create_signal('pybbs', ['Category'])
+
+        # Adding model 'Thread'
+        db.create_table('pybbs_thread', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('cate', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['pybbs.Category'])),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('content', self.gf('django.db.models.fields.TextField')()),
+            ('hits', self.gf('django.db.models.fields.IntegerField')(default=0)),
+            ('create_at', self.gf('django.db.models.fields.DateTimeField')()),
+            ('update_at', self.gf('django.db.models.fields.DateTimeField')()),
+        ))
+        db.send_create_signal('pybbs', ['Thread'])
+
+        # Adding model 'Reply'
+        db.create_table('pybbs_reply', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('thread', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['pybbs.Thread'])),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('content', self.gf('django.db.models.fields.TextField')()),
+            ('create_at', self.gf('django.db.models.fields.DateTimeField')()),
+            ('update_at', self.gf('django.db.models.fields.DateTimeField')()),
+        ))
+        db.send_create_signal('pybbs', ['Reply'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'Category'
+        db.delete_table('pybbs_category')
+
+        # Deleting model 'Thread'
+        db.delete_table('pybbs_thread')
+
+        # Deleting model 'Reply'
+        db.delete_table('pybbs_reply')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'pybbs.category': {
+            'Meta': {'object_name': 'Category'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'}),
+            'sort': ('django.db.models.fields.IntegerField', [], {'default': '1', 'max_length': '10'})
+        },
+        'pybbs.reply': {
+            'Meta': {'object_name': 'Reply'},
+            'content': ('django.db.models.fields.TextField', [], {}),
+            'create_at': ('django.db.models.fields.DateTimeField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'thread': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pybbs.Thread']"}),
+            'update_at': ('django.db.models.fields.DateTimeField', [], {}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'pybbs.thread': {
+            'Meta': {'object_name': 'Thread'},
+            'cate': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pybbs.Category']"}),
+            'content': ('django.db.models.fields.TextField', [], {}),
+            'create_at': ('django.db.models.fields.DateTimeField', [], {}),
+            'hits': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'update_at': ('django.db.models.fields.DateTimeField', [], {}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        }
+    }
+
+    complete_apps = ['pybbs']
--- pybbs/models.py
+++ pybbs/models.py
@@ -1,15 +1,27 @@
 #! coding:utf-8
 from django.db import models
-from django.contrib.auth import User
-#from django.contrib import admin
+from django.contrib.auth.models import User
+
+# 彪哥，虽然你的习惯是在model里面加入admin
+# but，Django的约束是放在admin.py 里面。
+# 我不想说明神马，只是希望这次你就依了我吧。
+
+# 彪哥必看
+# 关于我加入的south app 这是在Django里面的migration
+# 你也不想每次都要先删除数据库以后 manager.py syncdb吧？
+# 所以给你总结出来就是
+# ./manage.py schemamigration [app] --auto
+# ./manage.py migrate [app]
+# 有神马问题立刻联系我，如果没有问题，表示同意的话请删除注释
 
 class Category(models.Model):
     name = models.CharField('名称' , max_length = 100)
-    slug = models.SlugField ()
+    slug = models.SlugField ( u"短地址", max_length = 100 )
     sort = models.IntegerField('排序' , max_length = 10 , default = 01)
 
 class Thread(models.Model):
     cate = models.ForeignKey(Category)
+    slug = models.SlugField ( u"短地址", max_length = 100, null = True, blank = True )
     title = models.CharField('标题' , max_length = 255)
     user = models.ForeignKey(User)
     content = models.TextField('内容')
