2680b39565a9d3bc7345bc98cfc6d976e8930a0a
CJ <cn.imcj@gmail.com>
2011-01-07 23:04:44 +0800

    use slug

--- bbs/models.py
+++ bbs/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth import User
 
 class Category(models.Model):
     name = models.CharField('名称' , max_length = 100)
-    linkname = models.CharField('连接名' , max_length = 100)
+    slug = models.SlugField ()
     sort = models.IntegerField('排序' , max_length = 10 , default = 01)
 
 class Thread(models.Model):
