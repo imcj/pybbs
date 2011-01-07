f3aff0ef28fdf092ef6cbcd46ac772a32e5b79f1
Eric Wong <sd49124800@gmail.com>
2011-01-07 22:05:27 +0800

    add some modifies

--- bbs/models.py
+++ bbs/models.py
@@ -1,6 +1,7 @@
 #! coding:utf-8
 from django.db import models
-from django.contrib import admin
+from django.contrib.auth import User
+#from django.contrib import admin
 
 class Category(models.Model):
     name = models.CharField('名称' , max_length = 100)
@@ -8,7 +9,17 @@ class Category(models.Model):
     sort = models.IntegerField('排序' , max_length = 10 , default = 01)
 
 class Thread(models.Model):
-    pass
+    cate = models.ForeignKey(Category)
+    title = models.CharField('标题' , max_length = 255)
+    user = models.ForeignKey(User)
+    content = models.TextField('内容')
+    hits = models.IntegerField('浏览量' , default = 0)
+    create_at = models.DateTimeField('发表时间')
+    update_at = models.DateTimeField('最后修改时间')
 
 class Reply(models.Model):
-    pass
+    thread = models.ForeignKey(Thread)
+    user = models.ForeignKey(User)
+    content = models.TextField('内容')
+    create_at = models.DateTimeField('发表时间')
+    update_at = models.DateTimeField('最后修改时间')
--- bbs/views.py
+++ bbs/views.py
@@ -1,1 +1,25 @@
-# Create your views here.
+#! coding:utf-8
+from django.http import HttpResponseRedirect , HttpRequest , HttpResponse
+from django.shortcuts import render_to_response
+from django.contrib.auth import User
+from org_51py.bbs.models import *
+
+def index(req):
+    pass
+
+def category(req , cate):
+    pass
+
+def thread(req , id):
+    try:
+        tid = int(id)
+        td = Thread.objects.get(id=id)
+        return HttpResponse(td.title)
+    except:
+        return HttpResponseRedirect('/error/')
+
+def reply(req , tid):
+    if( req.method == 'POST' ):
+        pass
+    else:
+        return render_to_response('reply.html' , locals())
